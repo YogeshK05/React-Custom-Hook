cde4f82b2a9bf518d71bf441191bb73ff137e0c6
YogeshK05 <y.b.kadam01@gmail.com>
2026-01-15 01:24:39 +0530

    first commit

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export function useFetch(fetchFn, initialValue) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [fetchedData, setFetchedData] = useState(initialValue);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchFn();
+        setFetchedData(data);
+        setIsFetching(false);
+      } catch (error) {
+        setError({
+          message: error.message || "Could not fetch data.",
+        });
+        setIsFetching(false);
+      }
+    }
+
+    fetchData();
+  }, [fetchFn]);
+
+  return { isFetching, fetchedData, error, setFetchedData };
+}
